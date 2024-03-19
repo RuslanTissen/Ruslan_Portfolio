c10ad2446c6cbd19f4921c6e75112086796ef57f
SFERAS <rthyssen73@gmail.com>
2024-03-19 20:23:20 +0100

    nav done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -60,34 +60,32 @@ function Header() {
 		};
 	}, [languageRef]);
 
-	const workRef = useRef(null);
-	useEffect(() => {
-		function handleClickOutside(event) {
-			if (workRef.current && !workRef.current.contains(event.target)) {
-				setShowWorks(false);
-			}
-		}
+	// const workRef = useRef(null);
+	// useEffect(() => {
+	// 	function handleClickOutside(event) {
+	// 		if (workRef.current && !workRef.current.contains(event.target)) {
+	// 			setShowWorks(false);
+	// 		}
+	// 	}
 
-		document.addEventListener("mousedown", handleClickOutside);
-		return () => {
-			document.removeEventListener("mousedown", handleClickOutside);
-		};
-	}, [workRef]);
+	// 	document.addEventListener("mousedown", handleClickOutside);
+	// 	return () => {
+	// 		document.removeEventListener("mousedown", handleClickOutside);
+	// 	};
+	// }, [workRef]);
 
 	return (
 		<div className='header' id='header'>
 			<ul className="header__smart-cont" ref={menuRef}>
-				<li className='header__smart-cont--logo-box'>
-					<NavLink to="*" onClick={scrollToTheTop}>	TISSEN</NavLink>
-				</li>
+				<a className='logo' href='#header'>	TISSEN</a>
 				<li className='header__smart-cont--hamburger-box' onClick={toggleMenu}>
 					{active ? <RxHamburgerMenu /> : <RxCross1 />}
 				</li>
 				{show && <ul className="header__smart-cont--humb-cont" onClick={toggleMenu}>
-					<li className='humb-link'><NavLink to='*' onClick={scrollToTheTop}>Home</NavLink> </li>
-					<li className='humb-link'><NavLink to="*" onClick={scrollToTheTop}>About</NavLink> </li>
-					<li className='humb-link'><NavLink to="*" onClick={scrollToTheTop}>My works</NavLink> </li>
-					<li className='humb-link'><NavLink to="*" onClick={scrollToTheTop}>Contact</NavLink> </li>
+					<li className='humb-link'><a to='*' onClick={scrollToTheTop}>Home</a> </li>
+					<li className='humb-link'><a href='#about'>About</a> </li>
+					<li className='humb-link'><a href='#works'>My works</a> </li>
+					<li className='humb-link'><a href='#contact'>Contact</a> </li>
 					<li className='humb-link humb-link--lang'>
 						<button className="button-esp" value="es" onClick={(e) => setLang(e.target.value)}>ES</button>
 						<button className="button-eng" value="en" onClick={(e) => setLang(e.target.value)}>EN</button>
@@ -95,33 +93,19 @@ function Header() {
 				</ul>}
 			</ul>
 
-			<div className='header__pc-cont'>
-				<div className="header__pc-cont--ul-cont">
-					<ul className='ul-self'>
-						<li className='li-self li1'>
-							<NavLink to="*" onClick={scrollToTheTop}>
-								{/* <span>R</span> */}
-								TISSEN</NavLink>
-						</li>
-					</ul>
+			<div className='header__pc'>
+				<div className="header__pc-cont">
+					<a className='logo-pc' href='#header'>TISSEN</a>
 
-					<ul className="second-ul">
-						<li className='li-self li1'><NavLink to='*' onClick={scrollToTheTop}>Home</NavLink></li>
-						<li className='li-self li2'><NavLink to="*" onClick={scrollToTheTop}>About</NavLink> </li>
-						<li className="dropdown" ref={workRef} data-dropdown onClick={toggleWorks}>
-							<button className='drpdw-works--button' data-dropdown-button>My works</button>
-							{showWorks && <div className='drpdwn-works'>
-								<NavLink className="prt-work1" to="*">Project-1</NavLink>
-								<NavLink className="prt-work2" to="*">Project-2</NavLink>
-								<NavLink className="prt-work3" to="*">Project-3</NavLink>
-								<NavLink className="prt-work4" to="*">Project-4</NavLink>
-							</div>}
-						</li>
-						<li className='li-self li4'><a to="*" onClick={scrollToTheTop}>Contact</a> </li>
+					<ul className="nav">
+						<li className='li-self li1'><a href='#header'>Home</a></li>
+						<li className='li-self li2'><a href='#about'>About</a> </li>
+						<li className="li-self li3" ><a href="#works">My Works</a>	</li>
+						<li className='li-self li4'><a href='#contact'>Contact</a> </li>
 						<li className="dropdown" ref={languageRef} data-dropdown onClick={toggleLanguages}>
-							<button className='link' data-dropdown-button><BsGlobe /></button>
+							<div className='dropdown__link' data-dropdown-button><BsGlobe /></div>
 							{showLanguages && <div className='drpdwn-lang'>
-								<button className="button-esp" value="es" onClick={(e) => setLang(e.target.value)}>ES</button>
+								<button className="button-esp" value="es" onClick={(e) => setLang(e.target.value)}>GR</button>
 								<button className="button-eng" value="en" onClick={(e) => setLang(e.target.value)}>EN</button>
 							</div>}
 						</li>
