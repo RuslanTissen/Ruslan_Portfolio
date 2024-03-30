4664a7996cdc909baa39259d782ec2d1e18f1cd4
SFERAS <rthyssen73@gmail.com>
2024-03-31 00:51:37 +0100

    edvert trans done

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -20,16 +20,28 @@ const trans = {
 		fastProject: "This page is optimized for both mobile phones and computers, and it's available in two languages. It loads quickly, allowing you to access content promptly. With its sleek and modern design, it offers an enjoyable user experience.",
 		fastTech: "Technologies: JavaScript, ReactJS, SASS, Canva, Material-UI (MUI), React Icons",
 
-		//Stretching
-		stretching: "streching",
-		stretchingCourse: "course",
-		stretchingText: "The Stretching Course, developed by Pipi, offers a unique approach to improving flexibility and well-being.		With Pipi's expertise, participants are guided through innovative stretching techniques designed to enhance range of motion and reduce muscle tension.		This course empowers individuals to prioritize self-care and unlock their body's potential for improved mobility and overall health.",
-		stretchingAvailable: "Available in both",
-		stretchingSpanish: "Spanish",
-		stretchingAnd: "and",
-		stretchingEnglish: "English",
-		stretchingStart: "start now",
-		stretchingBased: "based on your needs*",
+		//advertising
+		advertSmall: "Small",
+		advertLanding: "Projects: Landing page",
+		advertLandingText: "A landing page is like a storefront for your website. It's the first thing people see when they visit your site. We specialize in creating landing pages for small projects, like introducing a new product or service. From designing to deployment, we handle every step of the process, ensuring your landing page is tailored to your needs and goals. Our landing pages are designed to grab attention, provide important information, and encourage visitors to take action, like signing up or making a purchase. With our expertise, we'll make sure your landing page is engaging, easy to navigate, and optimized for success. Let's build something amazing together!",
+		advertWebsite: "Website Development for",
+		advertMid: "Mid-Sized",
+		advertProjects: "Projects",
+		advertWhen: "When your project is growing and needs more than just a landing page, our mid-sized website development service is here for you. We specialize in crafting websites that are perfect for businesses and projects that require a bit more complexity and functionality. From building a sleek and professional design to implementing features like e-commerce functionality, contact forms, and content management systems, we handle it all. Our team guides you through every step of the process, from planning and design to development and launch. With our expertise, your mid-sized project will have a powerful online presence that engages visitors and drives results. Let's bring your vision to life!",
+		advertCustom: "Custom Web Solutions for",
+		advertLarge: "Large",
+		advertFor: "For ambitious projects that demand a truly bespoke approach, our custom web solutions are designed to meet your unique needs and exceed your expectations. We specialize in creating robust and scalable websites and web applications that can handle high volumes of traffic and complex functionalities. Whether you're launching a large-scale e-commerce platform, a social networking site, or an enterprise-level web application, we have the expertise to turn your vision into reality. Our team of skilled developers, designers, and project managers will work closely with you to understand your goals and deliver a solution that aligns perfectly with your requirements. From concept to execution, we're committed to delivering top-quality results that propel your project to success. Let's build something extraordinary together!",
+		advertMaintenance: "Website Maintenance and",
+		advertSupport: "Support",
+		advertOnce: "Once your website is up and running, it requires regular maintenance to ensure it stays secure, up-to-date, and performing at its best. Our website maintenance and support services are here to take the hassle out of managing your online presence, so you can focus on growing your business. From software updates and security patches to content updates and troubleshooting, our team handles it all, keeping your website running smoothly around the clock. Whether you need occasional assistance or ongoing support, we offer flexible maintenance plans to fit your needs and budget. With our proactive approach, you can rest easy knowing your website is in good hands. Let us take care of the technical details while you focus on what you do best.",
+		advertShopify: "Shopify Development Services: Build Your",
+		advertOnline: "Online Store",
+		advertReady: "Ready to take your business online? Our Shopify development services are here to help you create a stunning online store quickly and effortlessly. Shopify is a powerful platform that allows you to sell products online with ease, and we're experts at harnessing its full potential. From setting up your store and customizing the design to integrating payment gateways and optimizing for search engines, we handle every aspect of your Shopify store development. Whether you're a small boutique or a large enterprise, we tailor our services to fit your needs, ensuring your online store reflects your brand perfectly and delivers a seamless shopping experience for your customers. Let's turn your e-commerce dreams into reality with Shopify!",
+		advertCustomWeb: "Custom Web",
+		advertDesign: "Design",
+		advertServices: "Services: Your Vision, Our Expertise",
+		advertYour: "Your website is the face of your brand online, and our custom web design services are here to ensure it makes a lasting impression. We believe that every business is unique, and your website should reflect that. Our team of talented designers works closely with you to understand your brand, your goals, and your audience, crafting a custom-designed website that's as unique as your business. From sleek and modern designs to bold and creative layouts, we bring your vision to life with attention to detail and pixel-perfect precision. Whether you need a brand-new website or a redesign of your existing site, we're here to deliver a stunning, user-friendly experience that sets you apart from the competition. Let's create something extraordinary together!",
+
 
 		// CONTACT
 		contact: "MY Contacts",
@@ -59,6 +71,28 @@ const trans = {
 		simpleTech: "Technologien: JavaScript, ReactJS, SASS, Canva",
 		fastProject: "Diese Seite ist für sowohl Mobiltelefone als auch Computer optimiert und ist in zwei Sprachen verfügbar. Sie lädt schnell, sodass Sie schnell auf Inhalte zugreifen können. Mit ihrem schlanken und modernen Design bietet sie ein angenehmes Benutzererlebnis.",
 		fastTech: "Technologien: JavaScript, ReactJS, SASS, Canva, Material-UI (MUI), React Icons",
+
+		//advertising
+		advertSmall: "Klein",
+		advertLanding: "Projekte: Landingpage",
+		advertLandingText: "Eine Landingpage ist wie ein Schaufenster für Ihre Website. Es ist das Erste, was die Leute sehen, wenn sie Ihre Seite besuchen. Wir sind spezialisiert auf die Erstellung von Landingpages für kleine Projekte, wie die Einführung eines neuen Produkts oder Dienstes. Vom Design bis zur Bereitstellung kümmern wir uns um jeden Schritt des Prozesses und stellen sicher, dass Ihre Landingpage Ihren Bedürfnissen und Zielen entspricht. Unsere Landingpages sind darauf ausgelegt, Aufmerksamkeit zu erregen, wichtige Informationen bereitzustellen und Besucher dazu zu ermutigen, Maßnahmen zu ergreifen, wie beispielsweise die Anmeldung oder den Kauf. Mit unserem Fachwissen stellen wir sicher, dass Ihre Landingpage ansprechend, benutzerfreundlich und auf Erfolg optimiert ist. Lassen Sie uns gemeinsam etwas Unglaubliches erschaffen!",
+		advertWebsite: "Webseitenentwicklung für",
+		advertMid: "mittelgroße",
+		advertProjects: "Projekte",
+		advertWhen: "Wenn Ihr Projekt wächst und mehr als nur eine Landingpage benötigt, steht Ihnen unser mittelgroßer Website-Entwicklungsservice zur Verfügung. Wir haben uns auf die Erstellung von Websites spezialisiert, die perfekt für Unternehmen und Projekte sind, die etwas mehr Komplexität und Funktionalität erfordern. Vom Aufbau eines schlanken und professionellen Designs bis zur Implementierung von Funktionen wie E-Commerce-Funktionalität, Kontaktformularen und Content-Management-Systemen übernehmen wir alles. Unser Team begleitet Sie durch jeden Schritt des Prozesses, von der Planung und Gestaltung bis zur Entwicklung und Einführung. Mit unserer Expertise wird Ihr mittelgroßes Projekt eine starke Online-Präsenz haben, die Besucher begeistert und Ergebnisse erzielt. Lassen Sie uns Ihre Vision zum Leben erwecken!",
+		advertCustom: "Maßgeschneiderte Weblösungen für",
+		advertLarge: "große",
+		advertFor: "Für ehrgeizige Projekte, die einen maßgeschneiderten Ansatz erfordern, sind unsere individuellen Weblösungen darauf ausgelegt, Ihre einzigartigen Anforderungen zu erfüllen und Ihre Erwartungen zu übertreffen. Wir sind spezialisiert auf die Entwicklung robuster und skalierbarer Websites und Webanwendungen, die auch hohe Besucherzahlen und komplexe Funktionen bewältigen können. Egal, ob Sie eine groß angelegte E-Commerce-Plattform, ein soziales Netzwerk oder eine Unternehmens-Webanwendung starten möchten, wir verfügen über das Know-how, um Ihre Vision in die Realität umzusetzen. Unser Team aus erfahrenen Entwicklern, Designern und Projektmanagern wird eng mit Ihnen zusammenarbeiten, um Ihre Ziele zu verstehen und eine Lösung zu liefern, die perfekt zu Ihren Anforderungen passt. Vom Konzept bis zur Umsetzung sind wir bestrebt, hochwertige Ergebnisse zu liefern, die Ihr Projekt zum Erfolg führen. Lassen Sie uns gemeinsam etwas Außergewöhnliches schaffen!",
+		advertMaintenance: "Website-Wartung und",
+		advertSupport: "-Support",
+		advertOnce: "Sobald Ihre Website online ist, benötigt sie regelmäßige Wartung, um sicherzustellen, dass sie sicher, aktuell und optimal funktioniert. Unsere Website-Wartungs- und Support-Services sind hier, um Ihnen die Mühe bei der Verwaltung Ihrer Online-Präsenz abzunehmen, damit Sie sich auf das Wachstum Ihres Unternehmens konzentrieren können. Von Software-Updates und Sicherheits-Patches bis hin zu Inhaltsaktualisierungen und Fehlerbehebungen kümmert sich unser Team um alles, damit Ihre Website rund um die Uhr reibungslos läuft. Ob Sie gelegentliche Unterstützung oder kontinuierliche Unterstützung benötigen, wir bieten flexible Wartungspläne, die Ihren Anforderungen und Ihrem Budget entsprechen. Mit unserem proaktiven Ansatz können Sie beruhigt sein, dass Ihre Website in guten Händen ist. Lassen Sie uns die technischen Details erledigen, während Sie sich auf das konzentrieren, was Sie am besten können.",
+		advertShopify: "Shopify-Entwicklungsdienstleistungen: Erstellen Sie Ihren",
+		advertOnline: "Online-Shop",
+		advertReady: "Bereit, Ihr Geschäft online zu bringen? Unsere Shopify-Entwicklungsdienste sind hier, um Ihnen zu helfen, schnell und mühelos einen atemberaubenden Online-Shop zu erstellen. Shopify ist eine leistungsstarke Plattform, die es Ihnen ermöglicht, Produkte online leicht zu verkaufen, und wir sind Experten darin, ihr volles Potenzial zu nutzen. Von der Einrichtung Ihres Geschäfts und der Anpassung des Designs bis zur Integration von Zahlungsgateways und der Optimierung für Suchmaschinen übernehmen wir jeden Aspekt der Entwicklung Ihres Shopify-Shops. Egal, ob Sie eine kleine Boutique oder ein großes Unternehmen sind, wir passen unsere Dienstleistungen an Ihre Bedürfnisse an und stellen sicher, dass Ihr Online-Shop Ihr Markenbild perfekt widerspiegelt und Ihren Kunden ein nahtloses Einkaufserlebnis bietet. Lassen Sie uns Ihre E-Commerce-Träume mit Shopify Wirklichkeit werden lassen!",
+		advertCustomWeb: "Maßgeschneiderte",
+		advertDesign: "Webdesign",
+		advertServices: "-Dienstleistungen: Ihre Vision, Unsere Expertise",
+		advertYour: "Ihr Website ist das Aushängeschild Ihrer Marke online, und unsere maßgeschneiderten Webdesign-Dienstleistungen sind hier, um sicherzustellen, dass sie einen bleibenden Eindruck hinterlässt. Wir glauben, dass jedes Unternehmen einzigartig ist, und Ihre Website sollte das widerspiegeln. Unser Team talentierter Designer arbeitet eng mit Ihnen zusammen, um Ihre Marke, Ihre Ziele und Ihr Publikum zu verstehen und eine maßgeschneiderte Website zu gestalten, die so einzigartig ist wie Ihr Unternehmen. Von schlanken und modernen Designs bis hin zu kühnen und kreativen Layouts setzen wir Ihre Vision mit Liebe zum Detail und pixelgenauer Präzision um. Ob Sie eine brandneue Website benötigen oder eine Überarbeitung Ihrer bestehenden Website, wir sind hier, um ein beeindruckendes, benutzerfreundliches Erlebnis zu bieten, das Sie von der Konkurrenz abhebt. Lassen Sie uns gemeinsam etwas Außergewöhnliches schaffen!",
 
 	}
 }
--- src/components/start/advertising/Advertising.jsx
+++ src/components/start/advertising/Advertising.jsx
@@ -1,44 +1,48 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./Advertising.scss"
+import { Context } from '../../Context.js'
+import trans from '../../Translate.js'
 
 function Advertising() {
+	const {lang}  = useContext(Context)
+
 	return (
 		<div className='advertising' id='adverts'>
 			<div className="advertising-cont">
 
 				<div className='small services'>
-					<h2><span>Small</span>  Projects: Landing page</h2>
-					<p>A landing page is like a storefront for your website. It's the first thing people see when they visit your site. We specialize in creating landing pages for small projects, like introducing a new product or service. From designing to deployment, we handle every step of the process, ensuring your landing page is tailored to your needs and goals. Our landing pages are designed to grab attention, provide important information, and encourage visitors to take action, like signing up or making a purchase. With our expertise, we'll make sure your landing page is engaging, easy to navigate, and optimized for success. Let's build something amazing together!</p>
+					<h2><span> {trans[lang].advertSmall}</span>  {trans[lang].advertLanding}</h2>
+					<p>{trans[lang].advertLandingText}</p>
 
 				</div>
 
 				<div className='middle services'>
-					<h2>Website Development for <span>Mid-Sized</span>  Projects</h2>
-					<p>When your project is growing and needs more than just a landing page, our mid-sized website development service is here for you. We specialize in crafting websites that are perfect for businesses and projects that require a bit more complexity and functionality. From building a sleek and professional design to implementing features like e-commerce functionality, contact forms, and content management systems, we handle it all. Our team guides you through every step of the process, from planning and design to development and launch. With our expertise, your mid-sized project will have a powerful online presence that engages visitors and drives results. Let's bring your vision to life!</p>
+					<h2>{trans[lang].advertWebsite} <span>{trans[lang].advertMid}</span>  {trans[lang].advertProjects}</h2>
+					<p>{trans[lang].advertWhen}</p>
 
 				</div>
 
 				<div className='big services'>
-					<h2>Custom Web Solutions for <span>Large</span>  Projects</h2>
-					<p>For ambitious projects that demand a truly bespoke approach, our custom web solutions are designed to meet your unique needs and exceed your expectations. We specialize in creating robust and scalable websites and web applications that can handle high volumes of traffic and complex functionalities. Whether you're launching a large-scale e-commerce platform, a social networking site, or an enterprise-level web application, we have the expertise to turn your vision into reality. Our team of skilled developers, designers, and project managers will work closely with you to understand your goals and deliver a solution that aligns perfectly with your requirements. From concept to execution, we're committed to delivering top-quality results that propel your project to success. Let's build something extraordinary together!</p>
+					<h2>{trans[lang].advertCustom} <span>{trans[lang].advertLarge}</span>  {trans[lang].advertProjects}</h2>
+					<p>{trans[lang].advertFor}</p>
 
 				</div>
 
 				<div className='ai services'>
-					<h2>Website Maintenance and <span>Support</span></h2>
-					<p>Once your website is up and running, it requires regular maintenance to ensure it stays secure, up-to-date, and performing at its best. Our website maintenance and support services are here to take the hassle out of managing your online presence, so you can focus on growing your business. From software updates and security patches to content updates and troubleshooting, our team handles it all, keeping your website running smoothly around the clock. Whether you need occasional assistance or ongoing support, we offer flexible maintenance plans to fit your needs and budget. With our proactive approach, you can rest easy knowing your website is in good hands. Let us take care of the technical details while you focus on what you do best.</p>
+					<h2>{trans[lang].advertMaintenance} <span>{trans[lang].advertSupport}</span></h2>
+					<p>{trans[lang].advertOnce}</p>
 
 				</div>
 
 				<div className='shopify services'>
-					<h2>Shopify Development Services: Build Your  <span> Online Store </span> </h2>
-					<p>Ready to take your business online? Our Shopify development services are here to help you create a stunning online store quickly and effortlessly. Shopify is a powerful platform that allows you to sell products online with ease, and we're experts at harnessing its full potential. From setting up your store and customizing the design to integrating payment gateways and optimizing for search engines, we handle every aspect of your Shopify store development. Whether you're a small boutique or a large enterprise, we tailor our services to fit your needs, ensuring your online store reflects your brand perfectly and delivers a seamless shopping experience for your customers. Let's turn your e-commerce dreams into reality with Shopify!</p>
+					<h2>{trans[lang].advertShopify}  <span> {trans[lang].advertOnline} </span> </h2>
+					<p>{trans[lang].advertReady}</p>
 
 				</div>
 
 				<div className='designed services'>
-					<h2>Custom Web <span>Design</span> Services: Your Vision, Our Expertise</h2>
-					<p>Your website is the face of your brand online, and our custom web design services are here to ensure it makes a lasting impression. We believe that every business is unique, and your website should reflect that. Our team of talented designers works closely with you to understand your brand, your goals, and your audience, crafting a custom-designed website that's as unique as your business. From sleek and modern designs to bold and creative layouts, we bring your vision to life with attention to detail and pixel-perfect precision. Whether you need a brand-new website or a redesign of your existing site, we're here to deliver a stunning, user-friendly experience that sets you apart from the competition. Let's create something extraordinary together!</p>
+					<h2>{trans[lang].advertCustomWeb} <span>{trans[lang].advertDesign}</span> {trans[lang].advertServices}</h2>
+					<p>{trans[lang].advertYour}</p>
 
 				</div>
 
