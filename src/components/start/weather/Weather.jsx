8f36fbb897f8db4163c37f1823b4d0d22d521845
SFERAS <rthyssen73@gmail.com>
2024-04-12 21:43:55 +0200

    fight with fetch

--- src/components/start/weather/Weather.jsx
+++ src/components/start/weather/Weather.jsx
@@ -1,23 +1,56 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./Weather.scss"
 
 function Weather() {
-	fetch("https://jsonplaceholder.typicode.com/posts")
-		.then(response => response.json())
-		.then(post => {
-			console.log(post)
-			const li = document.createElement("li")
-			li.innerText = post[1].title
-			document.querySelector("#weather").prepend(li)
-		})
-		.catch(error => console.log(error))
+	const [data, setData] = useState(null)
+
+	const url = `https://jsonplaceholder.typicode.com/posts`
+
+	useEffect(() => {
+		if (!data) return
+
+		fetch(url)
+			.then(response => response.json())
+			.then(post => {
+				setData(post)
+				data.forEach(post => {
+					const li = document.createElement("li")
+					li.innerText = post.title
+					document.querySelector("#posts").append(li)
+				})
+
+			})
+			.catch(error => console.log(error))
+	}, [])
+
+	// useEffect(() => {
+	// 	document.querySelector("#save").addEventListener("click", () => {
+	// 		const text = document.querySelector("#content").value
+
+	// 		const config = {
+	// 			method: "POST",
+	// 			body: JSON.stringify({
+	// 				title: text,
+	// 				body: 'bar',
+	// 				userId: 1
+	// 			}),
+	// 			headers: {
+	// 				'Content-type': 'application/json; charset=UTF-8',
+	// 			}
+	// 		}
+
+	// 		fetch(url)
+	// 	})
+	// }, [])
 
 	return (
 		<div className='weather' id='weather'>
 
 
+			<input type="text" id='content' />
+			<button id="save">Create post</button>
 
-
+			<ul id='posts' ></ul>
 
 
 		</div>
